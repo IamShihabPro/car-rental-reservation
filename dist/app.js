0ec63c175f5be01f36c0218e8e6a2d36e133db50
IamShihabPro <mshihab.pro@gmail.com>
2024-09-03 18:30:20 +0600

    read me file

--- dist/app.js
+++ dist/app.js
@@ -13,7 +13,7 @@ const app = (0, express_1.default)();
 //parsers
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ origin: ['http://localhost:5173'] }));
+app.use((0, cors_1.default)({ origin: ['https://car-rental-frontend-ashen.vercel.app'] }));
 // application routes
 app.use('/api', routes_1.default);
 // const test = (req: Request, res: Response) => {
